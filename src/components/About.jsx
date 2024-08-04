d50949e06ba703864fbffb57b9ade2c10914c4a6
Daniel Santos <dsaraujo718@gmail.com>
2024-08-04 03:06:42 -0300

    Editando seção sobre e alterando título da página

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -5,7 +5,6 @@ export default function About() {
     return (
         <div className="border-b border-neutral-900 pb-4">
             <h1 className="my-20 text-center text-4xl">Sobre
-                <span className="text-neutral-500"> Me</span>
             </h1>
             <div className="flex flex-wrap">
                 <motion.div
